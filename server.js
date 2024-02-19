d564ce9d744f5703e9d3551c7864fa4aa332956a
mh-lamine <mlamins.ngom@gmail.com>
2024-02-19 14:58:01 +0100

    Add cors package and update dependencies

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 mongoose.connect(process.env.DATABASE_URL);
@@ -10,6 +11,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
 app.use(express.json());
+app.use(cors());
 
 const appointmentsRouter = require("./routes/appointments");
 app.use("/appointments", appointmentsRouter);
