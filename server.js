e84729e5e9b121061adee55d8e484272215f8f1e
mh-lamine <mlamins.ngom@gmail.com>
2024-02-20 16:12:53 +0100

    Update database connection URL and server port

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
-mongoose.connect(
-  "mongodb+srv://mlaminsngom:G5nGeEs3HS1S4O7e@cluster0.rdm9fzk.mongodb.net/connectx?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
@@ -18,6 +16,6 @@ app.use(cors());
 const appointmentsRouter = require("./routes/appointments");
 app.use("/appointments", appointmentsRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("http://localhost:3000");
 });
